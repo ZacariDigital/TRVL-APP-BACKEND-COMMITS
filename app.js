1b55fee474a1a585aac8377632b0a75f3ff18ebc
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2021-12-15 14:07:00 +0000

    testing middleware

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
-const customMiddleware = ()=>{
+const customMiddleware = (req, res, next)=>{
     console.log("middleware executed")
     next()
 }
 
-app.use(customMiddleware)
 
-app.get('/',(reqm,res)=>{
+
+app.get('/',(req,res)=>{
+    console.log("home")
     res.send("hello")
+})
+
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
+    res.send("about page")
 })
 
 app.listen(PORT,()=>{
