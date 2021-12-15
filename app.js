527936df5c8a8e0bb80ea78acb096da5955a9a59
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2021-12-15 14:26:58 +0000

    mongoose connection
    implemented code to see if its connected, if not it will throw an error

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 5000
+const {MONGURI} = require('./keys')
 
-const customMiddleware = (req, res, next)=>{
-    console.log("middleware executed")
-    next()
-}
-
-
-
-app.get('/',(req,res)=>{
-    console.log("home")
-    res.send("hello")
+mongoose.connect(MONGURI)
+mongoose.connection.on('connected', ()=>{
+    console.log("connected to mongo")
+})
+mongoose.connection.on('error', (err)=>{
+    console.log("error connection", err)
 })
 
-app.get('/about',customMiddleware,(req,res)=>{
-    console.log("about")
-    res.send("about page")
-})
+
 
 app.listen(PORT,()=>{
     console.log("server is active", PORT)
