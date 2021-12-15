3818ab6a33979f861d6197c12efd134a977bdb66
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2021-12-15 13:58:58 +0000

    middleware
    modify the request before reaching the route handler

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const customMiddleware = ()=>{
+    console.log("middleware executed")
+}
+
+app.use(customMiddleware)
+
 app.get('/',(reqm,res)=>{
     res.send("hello")
 })
 
 app.listen(PORT,()=>{
-    console.log("server is active")
+    console.log("server is active", PORT)
 })
